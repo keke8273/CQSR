aef707ef0b0ecdae0698b61809bed3ef7c411afa
Ke Liu <ke.frank.liu@gmail.com>
2015-12-10 21:53:51 +1100

    create the CQRS.Infrastructure project. added a few basic classes and interface.

---
+++ CQRS/CQRS.Infrastructure/EventSourcing/IVersionedEvent.cs
@@ -0,0 +1,12 @@
+﻿using CQRS.Infrastructure.Messaging;
+
+namespace CQRS.Infrastructure.EventSourcing
+{
+    public interface IVersionedEvent : IEvent
+    {
+        /// <summary>
+        /// Gets the version of the event in the stream.
+        /// </summary>
+        int Version { get; }
+    }
+}
---
+++ CQRS/CQRS.Infrastructure/Messaging/IEvent.cs
@@ -0,0 +1,15 @@
+﻿
+namespace CQRS.Infrastructure.Messaging
+{
+    using System;
+    /// <summary>
+    /// Represents an event message.
+    /// </summary>
+    public interface IEvent
+    {
+        /// <summary>
+        /// Gets the source identifier of the source originating the event.
+        /// </summary>
+        Guid SourceId { get; }
+    }
+}
