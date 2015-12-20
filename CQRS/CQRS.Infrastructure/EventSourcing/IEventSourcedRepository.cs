3ef7002ca330d25f193c1ee52a00e31b36b1bee2
Frank Liu <ke.frank.liu@gmail.com>
2015-12-20 22:58:25 +1100

    added IEventSourceRepository

---
+++ CQRS/CQRS.Infrastructure/EventSourcing/IEventSourcedRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Infrastructure.EventSourcing
+{
+    public interface IEventSourcedRepository<T> where T: IEventSourced
+    {
+        T Find(Guid id);
+
+        T Get(Guid id);
+
+        /// <summary>
+        /// Saves the specified event sourced.
+        /// </summary>
+        /// <param name="eventSourced">The event sourced entity.</param>
+        /// <param name="correlationId">The correlation id that identifies the command that generates the event.</param>
+        void Save(T eventSourced, string correlationId);
+    }
+}
--- CQRS/CQRS.Infrastructure/EventSourcing/VersionedEvent.cs
+++ CQRS/CQRS.Infrastructure/EventSourcing/VersionedEvent.cs
@@ -5,6 +5,9 @@ namespace CQRS.Infrastructure.EventSourcing
 
     public abstract class VersionedEvent : IVersionedEvent
     {
+        /// <summary>
+        /// Gets the source identifier of the source originating the event.
+        /// </summary>
         public Guid SourceId { get; set; }
 
         public int Version { get; set; }
