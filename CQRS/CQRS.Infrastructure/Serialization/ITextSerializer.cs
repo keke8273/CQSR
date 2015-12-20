4702c5a97a0c7f73f7d14a79cf88eb22713953b0
Frank Liu <ke.frank.liu@gmail.com>
2015-12-20 11:07:33 +1100

    added message Serialization functions

---
+++ CQRS/CQRS.Infrastructure/Serialization/ITextSerializer.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+
+namespace CQRS.Infrastructure.Serialization
+{
+    public interface ITextSerializer
+    {
+        void Serialize(TextWriter writer, object graph);
+
+        object Deserialize(TextReader reader);
+    }
+}
