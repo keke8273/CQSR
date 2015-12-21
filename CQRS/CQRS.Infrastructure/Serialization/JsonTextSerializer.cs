56fe6b807fe52c2bee489088d93730f0d655faf9
Frank Liu <ke.frank.liu@gmail.com>
2015-12-21 17:28:17 +1100

    added Json serializer

---
+++ CQRS/CQRS.Infrastructure/Serialization/JsonTextSerializer.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CQRS.Infrastructure.Serialization
+{
+    public class JsonTextSerializer : ITextSerializer
+    {
+        private readonly JsonSerializer serializer;
+
+        public JsonTextSerializer()
+        {
+            serializer = JsonSerializer.Create(new JsonSerializerSettings
+                {
+                    TypeNameHandling = TypeNameHandling.All,
+                    TypeNameAssemblyFormat = System.Runtime.Serialization.Formatters.FormatterAssemblyStyle.Simple
+                });
+        }
+
+        public JsonTextSerializer(JsonSerializer serializer)
+        {
+            this.serializer = serializer;
+        }
+
+        public void Serialize(System.IO.TextWriter writer, object graph)
+        {
+            var jsonWriter = new JsonTextWriter(writer);
+#if DEBUG
+            jsonWriter.Formatting = Formatting.Indented;
+#endif
+            serializer.Serialize(jsonWriter, graph);
+
+            //Dispose the jsonWriter here will dispose the writer too.  
+            writer.Flush();
+        }
+
+        public object Deserialize(TextReader reader)
+        {
+            var jsonReader = new JsonTextReader(reader);
+
+            try
+            {
+                return this.serializer.Deserialize(jsonReader);
+            }
+            catch (JsonSerializationException e)
+            {
+
+                throw new SerializationException(e.Message, e);
+            }
+        }
+    }
+}
