b797ab3d0cecf7ca48bc03cee5b26dd676669047
Frank Liu <ke.frank.liu@gmail.com>
2015-12-22 22:28:38 +1100

    added EventProcessor implementation

---
+++ CQRS/CQRS.Infrastructure.Sql/Messaging/Handling/EventProcessor.cs
@@ -0,0 +1,26 @@
+﻿using CQRS.Infrastructure.Messaging;
+using CQRS.Infrastructure.Messaging.Handling;
+using CQRS.Infrastructure.Serialization;
+
+namespace CQRS.Infrastructure.Sql.Messaging.Handling
+{
+    public class EventProcessor : MessageProcessor, IEventHandlerRegistery
+    {
+        private readonly EventDispatcher _messageDispatcher = new EventDispatcher();
+
+        public EventProcessor(IMessageReceiver receiver, ITextSerializer serializer) : base(receiver, serializer)
+        {
+        }
+
+        protected override void ProcessMessage(object payload, string correlationId)
+        {
+            var @event = (IEvent) payload;
+            _messageDispatcher.DispatchMessage(@event, null, correlationId, "");
+        }
+
+        public void Register(IEventHandler eventHandler)
+        {
+            _messageDispatcher.Register(eventHandler);
+        }
+    }
+}
